2db91f3d8a8795fe4ccc2851f1a26a07ae02f9dd
DFW1N <45083490+DFW1N@users.noreply.github.com>
2023-09-19 12:40:50 +1000

    Create main.tf

---
+++ terraform/layers/modules/base/azurerm_resouce_group/main.tf
@@ -0,0 +1,16 @@
+#============================================================================#
+#                                                                            #
+#                       Date Created: 19/09/2023                             #
+#                     Author: Sacha Roussakis-Notter                         #
+#                                                                            #
+# ===========================================================================#
+
+module "resource_groups" {
+depends_on                      = []
+    source                      = "../../../../main/base/azurerm_resource_group"
+    resources                   = var.resources
+    resource_groups             = var.main.resource_groups
+    dateCreated                 = var.dateCreated
+    environment                 = var.environment
+    managedBy                   = var.managedBy
+}
