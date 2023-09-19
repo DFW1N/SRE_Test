6a859b3493ae29d390a2e51d89dc29c2fab0af3e
DFW1N <45083490+DFW1N@users.noreply.github.com>
2023-09-19 12:19:43 +1000

    Create provider.tf

---
+++ terraform/layers/assets/provider.tf
@@ -0,0 +1,31 @@
+#============================================================================#
+#                                                                            #
+#                       Date Created: 19/09/2023                             #
+#                     Author Sacha Roussakis-Notter                          #
+#                                                                            #
+# ===========================================================================#
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "= 3.18.0"
+    }
+  }
+
+  backend "azurerm" {}
+}
+
+provider "azurerm" {
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy = true
+    }
+    template_deployment {
+      delete_nested_items_during_deletion = true
+    }
+    virtual_machine {
+      delete_os_disk_on_deletion = true
+    }
+  }
+}
