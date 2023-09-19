4001cbd3b079ef44070253a7fcc0a3d85ea8dcf7
DFW1N <45083490+DFW1N@users.noreply.github.com>
2023-09-19 16:46:34 +1000

    Update azure_manual_deployment.sh

--- scripts/azure_manual_deployment.sh
+++ scripts/azure_manual_deployment.sh
@@ -146,6 +146,7 @@ terraform plan \
   -var="environment=$environment_prefix" \
   -var="managedBy=$managed_by" \
   -var="dateCreated=$dateTime" \
-  -var-file=<(cat terraform.tfvars resources.tfvars) \
+  -var-file=terraform.tfvars \
+  -var-file=resources.tfvars \
   -out=$1-$environment_prefix-plan.out
 
