76719cae81bd9a50a692405ef3a01fcf3717b2d6
DFW1N <45083490+DFW1N@users.noreply.github.com>
2023-09-19 13:21:58 +1000

    Update azure_manual_deployment.sh

--- scripts/azure_manual_deployment.sh
+++ scripts/azure_manual_deployment.sh
@@ -7,9 +7,9 @@
 
 #!/bin/bash
 
-# Log in to Azure using the service principal
-az login --service-principal --username "$service_principal_id" --password "$service_principal_secret" --tenant "$tenant_id"
+# Authenticate to Azure using environment variables
+az login --service-principal --username "$ARM_CLIENT_ID" --password "$ARM_CLIENT_SECRET" --tenant "$ARM_TENANT_ID"
 
 # Set the Azure subscription
-az account set --subscription "$subscription_id"
+az account set --subscription "$ARM_SUBSCRIPTION_ID"
 
