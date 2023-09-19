5f50e0bb7c545d9b9fca96821e9d7714a301b3d9
DFW1N <45083490+DFW1N@users.noreply.github.com>
2023-09-19 12:38:39 +1000

    Create outputs.tf

---
+++ terraform/main/base/azurerm_resource_group/outputs.tf
@@ -0,0 +1,24 @@
+#============================================================================#
+#                                                                            #
+#                       Date Created: 19/09/2023                             #
+#                     Author: Sacha Roussakis-Notter                         #
+#                                                                            #
+# ===========================================================================#
+
+###########
+# OUTPUTS #
+###########
+
+output "azure_resource_group_ids" {
+    value = flatten([ for v in azurerm_resource_group.resource_groups : v.id])
+    description = "Lists the outputs of the Resource Group Id's"
+}
+
+output "azure_resource_group_names" {
+    value = flatten([ for n in azurerm_resource_group.resource_groups : n.name])
+    description = "Lists the outputs of the resource group name"
+}
+
+output "management_lock_ids" {
+    value = try(module.management_locks.management_lock_ids, null)
+}
