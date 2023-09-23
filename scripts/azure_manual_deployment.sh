ab22bde91e06942dd3ca36d1edb9fd0b3a3c41d9
Sacha Roussakis-Notter <sacha.roussakis-notter1@defence.gov.au>
2023-09-23 15:09:47 +1000

    included the environment prefix in the terraform state file creation

--- scripts/azure_manual_deployment.sh
+++ scripts/azure_manual_deployment.sh
@@ -414,7 +414,7 @@ terraform init \
   -backend-config="client_secret=$ARM_CLIENT_SECRET" \
   -backend-config="subscription_id=$ARM_SUBSCRIPTION_ID" \
   -backend-config="tenant_id=$ARM_TENANT_ID" \
-  -backend-config="key=$1/terraform.tfstate" \
+  -backend-config="key=$1/$environment_prefix/terraform.tfstate" \
   -upgrade
 
 if [ $? -ne 0 ]; then
